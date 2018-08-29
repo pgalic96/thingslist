268021bb9c6862a90d75840763d2be9de5be4f6d
pgalic96 <p.galic96@gmail.com>
2018-08-29 14:44:30 +0200

    Added the_things_list app to installed apps and migrated Thing model

--- thingslist/settings.py
+++ thingslist/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'the_things_list.apps.TheThingsListConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

29730a7ea0d282de9ee5951f5b5223be18a2eed6
pgalic96 <p.galic96@gmail.com>
2018-09-03 17:38:19 +0200

    Cleared database, added requirements.txt

--- the_things_list/models.py
+++ the_things_list/models.py
@@ -2,13 +2,21 @@ from django.db import models
 from django.urls import reverse
 from django.utils import timezone
 
+"""
+Database model of Things, more precisely announcements
+"""
 
-# Create your models here.
+
 class Thing(models.Model):
+    # Title of the announcement
     title = models.CharField(max_length=200)
+    # Text of the announcement
     text = models.TextField()
+    # Author, identified by the e-mail field
     author = models.EmailField()
+    # last edit time, the announcements are sorted by this on the front page
     lastEdit = models.DateTimeField('Timestamp', default=timezone.now)
+    # random token, used for editing announcements
     random_str = models.CharField(max_length=32, unique=True)
 
     def get_absolute_url(self):
